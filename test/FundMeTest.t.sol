76101a12e612ba37d498316332ac9bf0f125c41c
Ivan Hrekov <iandrc9@gmail.com>
2026-02-25 21:05:50 +0100

    test: setup and run first FundMe test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.33;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeTest is Test {}
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUsdIsFive() public view {
+        assertEq(fundMe.MIN_USD(), 5e18);
+    }
+}
