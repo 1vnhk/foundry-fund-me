0a2a1a6d4b2c783e09632915d287743a88e5ae72
Ivan Hrekov <iandrc9@gmail.com>
2026-02-25 21:19:29 +0100

    test: test for FundMe owner

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -14,4 +14,8 @@ contract FundMeTest is Test {
     function testMinimumUsdIsFive() public view {
         assertEq(fundMe.MIN_USD(), 5e18);
     }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.I_OWNER(), address(this));
+    }
 }
