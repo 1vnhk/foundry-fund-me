c057d0f4a7dfec8fe6b0e0f4c1c3080ec01cb77c
Ivan Hrekov <iandrc9@gmail.com>
2026-03-01 19:11:22 +0100

    refactor: address issues

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.8.33;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+error PriceConverter__InvalidPrice();
+
 library PriceConverter {
     function getPrice(
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
         // Address of the Chainlink oracle ETH/USD on Sepolia: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         (, int256 price, , , ) = priceFeed.latestRoundData();
+        if (price <= 0) {
+            revert PriceConverter__InvalidPrice();
+        }
         return uint256(price) * (10 ** (18 - priceFeed.decimals()));
     }
 
@@ -17,7 +22,7 @@ library PriceConverter {
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) * (10 ** 18);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / (10 ** 18);
 
         return ethAmountInUsd;
     }
